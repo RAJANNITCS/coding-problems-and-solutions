15cd7196f3e5508c7899dd9beba15ea6ba7ddd17
RAJANNITCS <rajan.nit.cs@gmail.com>
2021-07-05 11:15:54 +0530

    add test file

---
+++ 1.array_problems/TestJava.java
@@ -0,0 +1,14 @@
+// This java program for testing perpost 
+
+import java.util.Scanner;
+
+class TestJava {
+
+    public static void main(String[] args) {
+        int value = 0;
+        Scanner intValue = new Scanner(System.in);
+        System.out.println("Enter value");
+        value = intValue.nextInt();
+        System.out.println("The value is "+ value);
+    }
+}
