b42ea10f0356bcc37094b2b3cd1918de1e036792
RAJANNITCS <rajan.nit.cs@gmail.com>
2021-07-09 14:51:36 +0530

    add transpose

--- 1.array_problems/LinearSearch.java
+++ 1.array_problems/LinearSearch.java
@@ -14,18 +14,20 @@ class LinearSearch {
         obj.initializeArray(obj);
         obj.display(obj);
         // index = obj.linearSearchLoop(obj);
-        index = obj.transposition(obj);
-        if(index < 0) {
+        // index = obj.transposition(obj);
+        index = obj.moveToFrontOrhead(obj);
+        if (index < 0) {
             System.out.println("The key value is not found");
-        }else {
-            System.out.println("The index is "+ index);
+        } else {
+            System.out.println("The index is " + index);
         }
 
-        index = obj.transposition(obj);
-        if(index < 0) {
+        // index = obj.transposition(obj);
+        index = obj.moveToFrontOrhead(obj);
+        if (index < 0) {
             System.out.println("The key value is not found");
-        }else {
-            System.out.println("The index is "+ index);
+        } else {
+            System.out.println("The index is " + index);
         }
     }
 
@@ -36,14 +38,14 @@ class LinearSearch {
         System.out.println("How many value you will enter");
         obj.length = obj.intValue.nextInt();
         System.out.println("Enter Array value");
-        for(int i = 0; i < obj.length; i++) {
+        for (int i = 0; i < obj.length; i++) {
             obj.arr[i] = obj.intValue.nextInt();
         }
     }
 
     void display(LinearSearch obj) {
         System.out.println("Array elements are");
-        for(int i = 0 ; i < obj.length; i++) {
+        for (int i = 0; i < obj.length; i++) {
             System.out.println(obj.arr[i]);
         }
     }
@@ -52,25 +54,40 @@ class LinearSearch {
         int keyValue = 0;
         System.out.println("Enter key value for search");
         keyValue = obj.intValue.nextInt();
-        for(int i = 0; i < obj.length; i++) {
+        for (int i = 0; i < obj.length; i++) {
             if (obj.arr[i] == keyValue) {
                 return i;
             }
         }
         return -1;
-        
+
     }
 
     int transposition(LinearSearch obj) {
         int keyValue = 0;
         System.out.println("Enter value for serarching");
         keyValue = obj.intValue.nextInt();
-        for(int i = 0; i < obj.length; i++) {
-            if(obj.arr[i] == keyValue) {
+        for (int i = 0; i < obj.length; i++) {
+            if (obj.arr[i] == keyValue) {
                 int temp = 0;
                 temp = obj.arr[i];
                 obj.arr[i] = obj.arr[i - 1];
                 obj.arr[i - 1] = temp;
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    int moveToFrontOrhead(LinearSearch obj) {
+        int keyValue = 0;
+        System.out.println("Enter value for searching");
+        keyValue = obj.intValue.nextInt();
+        for (int i = 0; i < obj.length; i++) {
+            if (obj.arr[i] == keyValue) {
+                int temp = obj.arr[i];
+                obj.arr[i] = obj.arr[0];
+                obj.arr[0] = temp;
                 return i;
             }
         }
