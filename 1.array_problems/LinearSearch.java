43156b1ac31482db392629051cbcb21327ae8d7e
RAJANNITCS <rajan.nit.cs@gmail.com>
2021-07-09 13:01:07 +0530

    fix close bug

--- 1.array_problems/LinearSearch.java
+++ 1.array_problems/LinearSearch.java
@@ -6,19 +6,39 @@ class LinearSearch {
     int length = 0;
     int size = 0;
     int arr[] = null;
+    Scanner intValue = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int index = 0;
+        LinearSearch obj = new LinearSearch();
+        obj.initializeArray(obj);
+        obj.display(obj);
+        // index = obj.linearSearchLoop(obj);
+        index = obj.transposition(obj);
+        if(index < 0) {
+            System.out.println("The key value is not found");
+        }else {
+            System.out.println("The index is "+ index);
+        }
+
+        index = obj.transposition(obj);
+        if(index < 0) {
+            System.out.println("The key value is not found");
+        }else {
+            System.out.println("The index is "+ index);
+        }
+    }
 
     void initializeArray(LinearSearch obj) {
-        Scanner intValue = new Scanner(System.in);
         System.out.println("Enter size of array");
-        obj.size = intValue.nextInt();
+        obj.size = obj.intValue.nextInt();
         obj.arr = new int[obj.size];
         System.out.println("How many value you will enter");
-        obj.length = intValue.nextInt();
+        obj.length = obj.intValue.nextInt();
         System.out.println("Enter Array value");
         for(int i = 0; i < obj.length; i++) {
-            obj.arr[i] = intValue.nextInt();
+            obj.arr[i] = obj.intValue.nextInt();
         }
-        intValue.close();
     }
 
     void display(LinearSearch obj) {
@@ -30,10 +50,8 @@ class LinearSearch {
 
     int linearSearchLoop(LinearSearch obj) {
         int keyValue = 0;
-        Scanner intValueNew = new Scanner(System.in);
         System.out.println("Enter key value for search");
-        keyValue = intValueNew.nextInt();
-        intValueNew.close();
+        keyValue = obj.intValue.nextInt();
         for(int i = 0; i < obj.length; i++) {
             if (obj.arr[i] == keyValue) {
                 return i;
@@ -43,16 +61,19 @@ class LinearSearch {
         
     }
 
-    public static void main(String[] args) {
-        int index = 0;
-        LinearSearch obj = new LinearSearch();
-        obj.initializeArray(obj);
-        obj.display(obj);
-        index = obj.linearSearchLoop(obj);
-        if(index < 0) {
-            System.out.println("The key value is not found");
-        }else {
-            System.out.println("The index is "+ index);
+    int transposition(LinearSearch obj) {
+        int keyValue = 0;
+        System.out.println("Enter value for serarching");
+        keyValue = obj.intValue.nextInt();
+        for(int i = 0; i < obj.length; i++) {
+            if(obj.arr[i] == keyValue) {
+                int temp = 0;
+                temp = obj.arr[i];
+                obj.arr[i] = obj.arr[i - 1];
+                obj.arr[i - 1] = temp;
+                return i;
+            }
         }
+        return -1;
     }
 }
