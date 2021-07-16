c42291a534c5ce20aca6b3c66ecb920173f01d10
RAJANNITCS <rajan.nit.cs@gmail.com>
2021-07-16 11:44:54 +0530

    fix

---
+++ 3.string_problems/DiffUsingNewAndLitral.java
@@ -0,0 +1,17 @@
+// A java program for showing difference between 
+// buy createing string using new or litrel
+
+
+class DiffUsingNewAndLitral {
+
+    public static void main(String[] args) {
+        String animal_1 = new String("Dog");
+        String animal_2 = new String("Dog");
+        String animal_5 = "Dog";
+        // String animal_3 = "cat";
+        // String animal_4 = "cat";
+        // System.out.println(animal_1 == animal_2);
+        // System.out.println(animal_3 == animal_4);
+        // System.out.println(animal_1 == animal_5);
+    }
+}
---
+++ 3.string_problems/StringTest.java
@@ -0,0 +1,11 @@
+// A java program for String testing
+
+class StringTest {
+    
+    public static void main(String[] args) {
+        String name = "rajan singh";
+        String anOtherName = new String("rohit singh");
+        System.out.println(name);
+        System.out.println(anOtherName);
+    }
+}
