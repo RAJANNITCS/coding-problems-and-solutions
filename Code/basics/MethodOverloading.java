eb1af117ee539decb39fdfdd6f5a293db2ab89d7
RAJANNITCS <rajan.nit.cs@gmail.com>
2021-08-24 07:52:40 +0530

    fix

--- Code/basics/MethodOverloading.java
+++ Code/basics/MethodOverloading.java
@@ -11,8 +11,8 @@ public class MethodOverloading {
 	}
 	
 	public static void main(String[] args) {
-		MethodOverloading m1 = new MethodOverloading();
-		m1.main(20);
+		// MethodOverloading m1 = new MethodOverloading();
+		// m1.main(20);
 	}
 
 	public static void main(int a) {
