b7ff6d1e8dd30e8e83c7257e2a9adaf6dedca755
RAJANNITCS <rajan.nit.cs@gmail.com>
2021-09-25 12:54:01 +0530

    fix f

--- 5.java_learning/HelloWorld.java
+++ 5.java_learning/HelloWorld.java
@@ -3,6 +3,7 @@
 
 class HelloWorld {
     public static void main(String[] args)  {
-        System.out.println("Hello World !!");
+        int a[] = {1,2,};
+        System.out.println(a.length);
     }
 }
